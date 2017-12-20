5133dfe1a5d62d48b8f21124bf884fc6818309f5
James Myers <jfmyers9@gmail.com>
2017-12-20 17:53:34 +0000

    Solution to Day 6

---
+++ six/main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/jfmyers9/advent-of-code-2017/helpers"
+)
+
+var input = flag.String("input", "", "Input to the puzzle")
+
+func main() {
+	flag.Parse()
+
+	if *input == "" {
+		panic("provide an input file")
+	}
+
+	f, err := os.Open(*input)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	s := bufio.NewScanner(f)
+
+	for s.Scan() {
+		line := s.Text()
+		elements := strings.Split(line, "\t")
+		numbers := convertElementsToInts(elements)
+		fmt.Printf("Num Cycles: %d\n", performRedistribution(numbers))
+		return
+	}
+}
+
+func convertElementsToInts(values []string) []int {
+	result := make([]int, len(values))
+	for i, s := range values {
+		result[i] = helpers.MustConvertToInt(s)
+	}
+
+	return result
+}
+
+func convertElementsToString(values []int) string {
+	result := make([]string, len(values))
+	for i, n := range values {
+		result[i] = strconv.Itoa(n)
+	}
+
+	return strings.Join(result, ",")
+}
+
+func performRedistribution(numbers []int) int {
+	seen := map[string]struct{}{}
+	seen[convertElementsToString(numbers)] = struct{}{}
+	moves := 0
+	for {
+		moves++
+
+		maxPos := 0
+		max := 0
+		for i, n := range numbers {
+			if n > max {
+				max = n
+				maxPos = i
+			}
+		}
+
+		numbers[maxPos] = 0
+		position := maxPos + 1
+		for i := max; i > 0; i-- {
+			if position >= len(numbers) {
+				position = 0
+			}
+
+			numbers[position]++
+			position++
+		}
+
+		key := convertElementsToString(numbers)
+		if _, ok := seen[key]; ok {
+			return moves
+		}
+
+		seen[key] = struct{}{}
+	}
+}
